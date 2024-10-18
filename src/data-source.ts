8d97f2c849298b580e3d96501071feb5c1e19211
Nguyễn Thanh Bình <binh.nguyen@nexlab.tech>
2024-10-18 14:49:31 +0700

    build: fix env mismatch and bugs on bcrypt

--- src/data-source.ts
+++ src/data-source.ts
@@ -4,8 +4,8 @@ import { User } from "./entities/user.entity.js";
 
 export const AppDataSource = new DataSource({
   type: "postgres",
-  host: "localhost",
-  port: 5432,
+  host: process.env.DB_HOST || "localhost",
+  port: process.env.DB_PORT ? parseInt(process.env.DB_PORT) : 5432,
   username: process.env.DB_USERNAME || "postgres",
   password: process.env.DB_PASSWORD || "postgres",
   database: process.env.DB_NAME || "student_management",
