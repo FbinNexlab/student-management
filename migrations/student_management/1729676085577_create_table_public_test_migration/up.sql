f70b81ab4f66e2823510f4382e8315b582b9444a
Nguyễn Thanh Bình <ntbinh21@clc.fitus.edu.vn>
2024-10-23 16:45:49 +0700

    feat: hosting hasura console via hasura cli, test migration and sync metadata with our console

---
+++ migrations/student_management/1729676085577_create_table_public_test_migration/up.sql
@@ -0,0 +1,1 @@
+CREATE TABLE "public"."test_migration" ("id" serial NOT NULL, PRIMARY KEY ("id") );COMMENT ON TABLE "public"."test_migration" IS E'create a new table to test migration through hasura console';
